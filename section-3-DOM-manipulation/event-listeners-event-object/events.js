51eb106eb0034350a8bb7d692f8d5e38685562e6
luke-h1 <luke.howsam@yahoo.com>
2020-07-21 20:23:07 +0100

    update to project

--- section-3-DOM-manipulation/event-listeners-event-object/events.js
+++ section-3-DOM-manipulation/event-listeners-event-object/events.js
@@ -6,3 +6,31 @@
 
 
 
+// Event target element 
+
+
+val1 = e.target ; 
+val1 = e.target.id ; 
+val1 = e.target.className ; 
+val1 = e.target.classList ; 
+
+
+// event type 
+
+val1 = e.type;  // can see what type of event it was 
+
+
+// timestamp 
+val1 = e.timestamp // get timestamp 
+
+
+// coordinates relative to the window. Y axis = vertical axis 
+val1 = e.clientY; 
+
+
+// x axis = horizontal axis 
+val1 = e.clientX; 
+
+// coords event relative to the element 
+val1 = e.offsetY //vertical 
+val1 = e.offsetX //horizontal 
