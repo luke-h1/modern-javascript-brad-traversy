e0de4300b2a7d06971f9e05310c2ffa3f7776535
luke-h1 <luke.howsam@yahoo.com>
2020-07-21 15:47:36 +0100

    update:

---
+++ section-3-DOM-manipulation/creating-elements/creating-elements.js
@@ -0,0 +1,33 @@
+// create element
+
+const li = document.createElement('li');
+
+// add class
+li.className = 'collection-item';
+
+// add ID
+li.id = 'new-item';
+
+// add attribute
+li.setAttribute('title', 'New Item');
+
+
+// create text node and append: 
+li.appendChild(document.createTextNode('Hello world!'))
+
+
+// create new link element 
+const link = document.createElement('a'); 
+
+// add classes
+link.className = 'delete-item secondary-content' 
+
+// add icon html 
+link.innerHTML = '<i class="fa fa-remove"></i>'
+
+// append link into li 
+li.appendChild(link);
+
+
+// append li as child to ul 
+document.querySelector('ul.collection').appendChild(li);
