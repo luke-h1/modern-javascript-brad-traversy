8c01b990c9081ac85861f7a6dabd292d8b6131c7
luke-h1 <luke.howsam@yahoo.com>
2020-07-27 19:43:43 +0100

    update

--- section-3-DOM-manipulation/event-bubbling-and-delegation/event-bubbling-and-delegation.js
+++ section-3-DOM-manipulation/event-bubbling-and-delegation/event-bubbling-and-delegation.js
@@ -14,12 +14,22 @@
 //     console.log('card');
 // });
 
-
 // document.querySelector('.col').addEventListener('click', function (e) {
 //     e.preventDefault()
 //     console.log('col');
 // });
 
+// EVENT DELEGATION
 
-// EVENT DELEGATION 
+// situation in which you would need to use event delegation
 
+// const delItem = document.querySelector('.delete-item');
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+    console.log(e.target);
+    if (e.target.parentElement.className === 'delete-item secondary-content') {
+    }
+}
