777fad4e0909b5d43eba14ea6273f0911b28604b
luke-h1 <luke.howsam@yahoo.com>
2020-08-06 17:09:14 +0100

    update'

---
+++ section-6-OOP-book-list-project/book-list/app.js
@@ -0,0 +1,47 @@
+// Book constructor
+// handles create book object
+
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI constructor
+// will contain prototype methods
+
+function UI() {}
+
+// add book to list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list'); 
+  // create tr element (table row)
+  const row = document.createElement('tr');   
+  // insert columns 
+  row.innerHTML = `
+    <td>${book.title}</td> 
+    <td>${book.author}</td> 
+    <td>${book.isbn}</td> 
+    <td><a href="" class="delete"><X</a></td>   
+  `;  
+
+  list.appendChild(row);
+};
+
+// event listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+    // get form values
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+
+    // instantiate book
+    const book = new Book(title, author, isbn);
+
+    // instantiate UI
+    const ui = new UI();
+    // console.log(ui)
+    // add book to list
+    ui.addBookToList(book);
+});
