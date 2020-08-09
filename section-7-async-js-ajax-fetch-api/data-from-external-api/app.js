4dcc9e50cf86e4fb086a76569d60cb7cb69b1b8b
luke-h1 <luke.howsam@yahoo.com>
2020-08-09 13:58:08 +0100

    update

---
+++ section-7-async-js-ajax-fetch-api/data-from-external-api/app.js
@@ -0,0 +1,28 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+    e.preventDefault();
+    const number = document.querySelector('input[type="number"]').value;
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            let output = '';
+            if (response.type === 'success') {
+                response.value.forEach(function (joke) {
+                    output += `
+                  <li>${joke.joke}</li>               
+                `;
+                });
+            } else {
+                output += '<li>Something went wrong</li>';
+            }
+
+            document.querySelector('.output').innerHTML = output;
+        
+        }
+    };
+
+    xhr.open('GET', `http://api.icnb.com/jokes/random/${number}`, true);
+    xhr.send();
+}
