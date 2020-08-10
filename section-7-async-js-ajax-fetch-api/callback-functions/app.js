9d48d7c8626f7ea93cf9879571bbfd962ca92e6b
luke-h1 <luke.howsam@yahoo.com>
2020-08-10 13:09:13 +0100

    update

---
+++ section-7-async-js-ajax-fetch-api/callback-functions/app.js
@@ -0,0 +1,49 @@
+const posts = [
+    {
+        title: 'post one ',
+        body: 'THIS is post one ',
+    },
+
+    {
+        title: 'post two',
+        body: 'this is post two',
+    },
+];
+
+// function createPost(post) {
+//     setTimeout(function () {
+//         posts.push(post);
+//     }, 2000);
+// }
+
+// function getPosts() {
+//     setTimeout(function () {
+//         let output = '';
+//         posts.forEach(function (post) {
+//             output += `<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     }, 2000);
+// }
+
+// createPost({title: 'post three', body: 'this is post three'})
+
+// getPosts();
+
+function createPost(post, callback) { 
+    setTimeout(function () {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+function getPosts(){
+  setTimeout(function(){
+    let output = ''; 
+    posts.forEach(function(post){
+      output += `<li>${post.title}</li>`; 
+    }); 
+    document.body.innerHTML = output;
+  }, 1000)
+}
+createPost({title: 'post three', body: 'this is post three'}, getPosts); 
