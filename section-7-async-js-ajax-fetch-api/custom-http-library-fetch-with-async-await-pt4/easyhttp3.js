92ec339fe61ad55104c77a5ea2f8995e8273786b
luke-h1 <luke.howsam@yahoo.com>
2020-08-11 20:41:32 +0100

    update to library

--- section-7-async-js-ajax-fetch-api/custom-http-library-fetch-with-async-await-pt4/easyhttp3.js
+++ section-7-async-js-ajax-fetch-api/custom-http-library-fetch-with-async-await-pt4/easyhttp3.js
@@ -9,34 +9,39 @@ class EasyHTTP {
         return responseData;
     }
 
-    async post(url, data) {}
+    async post(url, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const resData = await response.json();
+        return resData;
+    }
 
     async put(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'PUT',
-                headers: {
-                    'Content-type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            })
-                .then((res) => res.json())
-                .then((data) => resolve(data))
-                .then((err) => reject(err));
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
         });
+        const resData = await response.json();
+        return resData;
     }
 
     async delete(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'DELETE',
-                headers: {
-                    'Content-type': 'application/json',
-                },
-            })
-                .then((res) => res.json())
-                .then(() => resolve('Resource now deleted ✅'))
-                .catch((err) => reject(err));
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
         });
+        const resData = await 'RESOURCE DELETED ✅';
+        return resData;
     }
 }
