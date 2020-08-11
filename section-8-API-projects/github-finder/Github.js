819b0f489d8d84ed509db9f043dc00ef0b31591d
luke-h1 <luke.howsam@yahoo.com>
2020-08-12 00:05:20 +0100

    hard code client secret and client id

--- section-8-API-projects/github-finder/Github.js
+++ section-8-API-projects/github-finder/Github.js
@@ -1,16 +1,32 @@
-export default class Github {
-    constructor() {
-        this.client_ID = process.env.CLIENT_ID;
-        this.client_secret = process.env.CLIENT_SECRET;
+// require('dotenv').config();
+
+// class Github {
+//     constructor() {
+//         this.client_ID = process.env.CLIENT_ID;
+//         this.client_secret = process.env.CLIENT_SECRET;
+//     }
+//     async getUser(user) {
+//         const API_URL = `https://api.github.com/users/${user}?client_ID=${this.client.ID}&client_secret=${this.client_secret}`;
+//         const profileResponse = await fetch(API_URL);
+//         const profile = await profileResponse.json();
+//         return {
+//             profile,
+//         };
+//     }
+// }
+
+class Github{
+  constructor(){
+    this.client_id = '08963522b04ef051f1b8' ;
+    this.client_secret = 'cef7f16c9a2aeb072032790d125c3dd3ae4ae99e';
+  }
+
+  async getUser(user){
+    const profileResponse = await fetch (`https://api.github.com/users/${user}?client_ID=08963522b04ef051f1b8&client_secret=cef7f16c9a2aeb072032790d125c3dd3ae4ae99e`); 
+    
+    const profileData = await profileResponse.json(); 
+    return {
+      profile: profileData 
     }
-    async getUser(user) {
-        const API_URL = `https://api.github.com/users/${user}?client_ID=${this.client.ID}&client_secret=${this.client_secret}`;
-        const profileResponse = await fetch(API_URL);
-        const profile = await profileResponse.json();
-        return {
-            profile,
-        };
-    }
+  }
 }
-
-
--- section-8-API-projects/github-finder/app.js
+++ section-8-API-projects/github-finder/app.js
@@ -1,15 +1,13 @@
-import github from './Github'; 
 
-// search input
-const searchUser = document.getElementById('searchUser');
-
-searchUser.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    const userText = e.target.value;
-    if (userText !== '') {
-        github.getUser(userText).then((data) => {
-            console.log(data);
-        });
-    } else {
-    }
-});
+const github = new Github;
+const searchUser = document.getElementById('searchUser'); 
+searchUser.addEventListener('keyup', (e) => { 
+  const userText = e.target.value; 
+  if(userText !== ''){
+    console.log(userText);
+    github.getUser(userText)
+      .then((data) => { 
+        console.log(data)
+      })
+  }
+})
