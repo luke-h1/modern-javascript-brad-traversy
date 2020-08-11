058694c1dd0422e625fdba3d5c5772de046fb2c5
luke-h1 <luke.howsam@yahoo.com>
2020-08-11 14:13:39 +0100

    update

--- section-7-async-js-ajax-fetch-api/custom-http-library/easyhttp.js
+++ section-7-async-js-ajax-fetch-api/custom-http-library/easyhttp.js
@@ -8,14 +8,49 @@ easyHTTP.prototype.get = function (url, callback) {
     let self = this;
     this.http.onload = function () {
         if (self.http.status === 200) {
-            callback(self.http.responseText);
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error' + self.http.status);
         }
     };
     this.http.send();
 };
 
 // Make an HTTP POST req
+easyHTTP.prototype.post = function (url, data, callback) { 
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    this.http.send(JSON.stringify(data));
+    let self = this;
+    this.http.onload = function () {
+        callback(null, self.http.responseText); 
+    };
+};
+
 
 // Make an HTTP PUT req
+easyHTTP.prototype.put = function (url, data, callback) { 
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    this.http.send(JSON.stringify(data));
+    let self = this;
+    this.http.onload = function () {
+        callback(null, self.http.responseText); 
+    };
+};
+
+
 
 // Make an HTTP DELETE req
+easyHTTP.prototype.delete = function (url, callback) {
+    this.http.open('DELETE', url, true);
+    let self = this;
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            callback(null, 'post deleted');
+        } else {
+            callback('Error' + self.http.status);
+        }
+    };
+    this.http.send();
+};
