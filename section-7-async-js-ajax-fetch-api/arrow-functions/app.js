f84a0fd5b08f8837313c471fa65d1348dc8bfcef
luke-h1 <luke.howsam@yahoo.com>
2020-08-11 16:54:21 +0100

    update

---
+++ section-7-async-js-ajax-fetch-api/arrow-functions/app.js
@@ -0,0 +1,14 @@
+// ES5 syntax 
+const sayHello = function(name){
+  console.log('hello ' + name + '!'); 
+}
+sayHello('luke')
+
+
+// ES6 arrow function syntax 
+const sayHelloArrow = (name) => { 
+  console.log('hello ' + name + ' this is an arrow function !')
+}
+
+
+sayHelloArrow('luke')
