8977ef23b110c3f39a21b5393ac9c379da899853
luke-h1 <luke.howsam@yahoo.com>
2020-08-11 21:36:54 +0100

    update to github finder project

---
+++ section-8-API-projects/github-finder/Github.js
@@ -0,0 +1,16 @@
+class Github {
+    constructor() {
+        this.client_ID = process.env.CLIENT_ID;
+        this.client_secret = process.env.CLIENT_SECRET;
+    }
+    async getUser(user) {
+        const API_URL = `https://api.github.com/users/${user}?client_ID=${this.client.ID}&client_secret=${this.client_secret}`;
+        const profileResponse = await fetch(API_URL);
+        const profile = await profileResponse.json();
+        return {
+            profile,
+        };
+    }
+}
+
+
---
+++ section-8-API-projects/github-finder/app.js
@@ -0,0 +1,17 @@
+
+// init class
+const github = new Github
+
+// search input
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    const userText = e.target.value;
+    if (userText !== '') {
+        github.getUser(userText).then((data) => {
+            console.log(data);
+        });
+    } else {
+    }
+});
