2c084173709ac5d2f1bad4c0aa70f286a17f6e8a
luke-h1 <luke.howsam@yahoo.com>
2020-08-19 20:51:00 +0100

    update

--- section-12-calorie-tracker-module-pattern/app.js
+++ section-12-calorie-tracker-module-pattern/app.js
@@ -1,78 +1,80 @@
-// storage controller 
+// storage controller
 
-
-
-// item controller 
+// item controller
 const ItemCtrl = (function () {
-  // Item Constructor
-  const Item = function (id, name, calories) {
-    this.id = id;
-    this.name = name;
-    this.calories = calories;
-  }
+    // Item Constructor
+    const Item = function (id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    };
 
-  // Data State: 
-  const data = {
-    items: [
-      {
-        id: 0,
-        name: 'Steak',
-        calories: 1100
-      },
-      {
-        id: 1,
-        name: 'chocolate',
-        calories: 300
-      },
-      {
-        id: 2,
-        name: 'oreos',
-        calories: 200
-      }
-    ],
-    currentItem: null,
-    totalCalories: 0
-  }
-  return { 
-    getItems: function(){
-      return data.items; 
-    }, 
-    logData: function () {
-      return data; // {items: Array(3), currentItem: null, totalCalories: 0} 
-    }
-  }
+    // Data State:
+    const data = {
+        items: [
+            {
+                id: 0,
+                name: 'Steak',
+                calories: 1100,
+            },
+            {
+                id: 1,
+                name: 'chocolate',
+                calories: 300,
+            },
+            {
+                id: 2,
+                name: 'oreos',
+                calories: 200,
+            },
+        ],
+        currentItem: null,
+        totalCalories: 0,
+    };
+    return {
+        getItems: function () {
+            return data.items;
+        },
+        logData: function () {
+            return data; // {items: Array(3), currentItem: null, totalCalories: 0}
+        },
+    };
+})();
 
-}()); 
-
-
-
-
-
-// UI controller 
+// UI controller
 const UICtrl = (function () {
-  // public methods 
-  return { 
-    populateItemList: function(items){
-      let html = ''; 
-      
-    }
+    const UISelectors = {
+        itemList: '#item-list',
+    };
+    // public methods
+    return {
+        populateItemList: function (items) {
+            let html = '';
+            items.forEach((item) => {
+                html += `
+            <li id="item-${item.id}" class="collection-item">
+            <strong>${item.name}</strong> <em> ${item.calories} Calories</em> 
+            <a href="#" class="secondary-content"><i class="edit-item fa-fapencil"</i> 
+            </a> 
+            </li> 
+        `;
+            });
+            // Insert list items into DOM
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+    };
+})();
 
-  }
-}())
-
-
-
-// App Controller 
+// App Controller
 const AppCtrl = (function (itemCtrl, UICtrl) {
-  // Public methods 
-  return {
-    init: function () {
-      const items = itemCtrl.getItems();       // Fetch Items from data struct 
-      UICtrl.populateItemList(items);         // populate list w/ items 
-      
-    }
-  }
-}(ItemCtrl, UICtrl))
+    // Public methods
+    return {
+        init: function () {
+            const items = itemCtrl.getItems(); // Fetch Items from data struct
+            UICtrl.populateItemList(items); // populate list w/ items
+        },
+    };
+})(ItemCtrl, UICtrl);
 
-// init 
+// init
 AppCtrl.init();
