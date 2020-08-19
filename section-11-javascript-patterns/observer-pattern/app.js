4f091a2760eb7e9045360816c15f760a24aabfd7
luke-h1 <luke.howsam@yahoo.com>
2020-08-19 15:34:38 +0100

    update

---
+++ section-11-javascript-patterns/observer-pattern/app.js
@@ -0,0 +1,44 @@
+function EventObserver() {
+    this.observers = [];
+}
+
+EventObserver.prototype = {
+    subscribe: function (fn) {
+        this.observers.push(fn);
+        console.log(`You are subscribed to ${fn.name}`);
+    },
+    unsubscribe: function (fn) {
+        this.observers = this.observers.filter(function (item) {
+            // filter out from the list whatever matches the callback function. if there is no match, the callback gets to say on the list. the filter returns a new list and reassigns the list of observers.
+            if (item !== fn) {
+                return item;
+            }
+        });
+        console.log(`You are unsubscribed from ${fn.name}`);
+    },
+
+    fire: function () {
+        this.observers.forEach((item) => {
+            item.call();
+        });
+    },
+};
+
+const click = new EventObserver();
+
+document.querySelector('.sub-ms').addEventListener('click', function () {
+    click.subscribe(getCurrentMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function () {
+    click.unsubscribe(getCurrentMilliseconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function () {
+    click.fire();
+});
+
+// click handler
+const getCurrentMilliseconds = () => {
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+};
