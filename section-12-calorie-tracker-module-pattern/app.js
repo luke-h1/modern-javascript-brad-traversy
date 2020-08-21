4b3e9bc1c20cd72f40b8a18e3fc92e619f18e0fc
luke-h1 <luke.howsam@yahoo.com>
2020-08-21 16:17:49 +0100

    update logData function to actually work

--- section-12-calorie-tracker-module-pattern/app.js
+++ section-12-calorie-tracker-module-pattern/app.js
@@ -2,31 +2,66 @@
 ===================#
 STORAGE CONTROLLER # 
 ===================# 
-*/  
-
-
-
+*/
 
 /* 
 ================#
 ITEM CONTROLLER # 
 ================# 
-*/  
+*/
 
+// IFFY FUNCTION
 
+const ItemCtrl = (function () {
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  } 
+    const data = {
+      items: [
+        {
+          id: 0,
+          name: 'breakfast',
+          calories: 500,
+        },
+        {
+          id: 1,
+          name: 'lunch',
+          calories: 500,
+        },
+        {
+          id: 2,
+          name: 'dinner',
+          calories: 500,
+        },
+      ],
+      currentItem: null,
+      totalCalories: 0,
+    }
+
+    return { 
+      logData: function(){
+        return data;
+      }
+    }
+  
+    
+
+})();
 
 /* 
 ================#
 UI CONTROLLER ### 
 ================# 
-*/  
+*/
 
-
-
+const UICtrl = (function () {})();
 
 /* 
 ================#
 APP CONTROLLER # 
 ================# 
-*/  
+*/
 
+const App = (function (ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
