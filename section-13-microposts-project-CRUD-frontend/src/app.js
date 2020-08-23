44df0148217aef213857a5e32280ae712bd2a2f9
luke-h1 <luke.howsam@yahoo.com>
2020-08-23 16:27:58 +0100

    update

--- section-13-microposts-project-CRUD-frontend/src/app.js
+++ section-13-microposts-project-CRUD-frontend/src/app.js
@@ -1,10 +1,25 @@
-const greeting = 'Hello World';
+/*
+=======================
+common JS MODULE SYNTAX
+=======================
+*/ 
+// const person = require('./common-js');  
+// console.log(person)  
+
+
+
+
+
+/*
+=======================
+ES2015 MODULE SYNTAX 
+=======================
+*/ 
+// import { person , sayHello} from './es2015-modules'; 
+// import * as mod from './es2015-modules'; // IMPORT EVERYTHING 
+// console.log(mod.person) 
+// console.log(mod.sayHello());
+
+import greeting from './es2015-modules'; 
+
 console.log(greeting);
-
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
-
-getData('https://jsonplaceholder.typicode.com/posts');
