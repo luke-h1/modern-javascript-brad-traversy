e9b702bab94ca242b0219d16db85594acd934b4a
luke-h1 <luke.howsam@yahoo.com>
2020-08-24 14:37:11 +0100

    update

--- section-13-microposts-project-CRUD-frontend/src/app.js
+++ section-13-microposts-project-CRUD-frontend/src/app.js
@@ -1,12 +1,55 @@
 import { http } from './http';
-import { ui } from './ui'; 
-// GET POSTS ON DOM LOAD 
-document.addEventListener('DOMContentLoaded', getPosts); 
+import { ui } from './ui';
 
+// GET POSTS ON DOM LOAD
+document.addEventListener('DOMContentLoaded', getPosts);
 
-function getPosts(){
-  http.get('http://localhost:3000/posts') 
-    .then(data => ui.showPosts(data))  
-    .catch(err => console.log(err))
+// LISTEN FOR POST
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// LISTEN FOR DELETE
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// GET POST
+function getPosts() {
+  http
+    .get('http://localhost:3000/posts')
+    .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
 }
 
+// ADD A POST
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  const data = {
+    title,
+    body,
+  };
+  // CREATE POST REQ
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch((err) => console.log(err));
+}
+
+// DELETE A POST
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) { 
+    const id = e.target.parentElement.dataset.id;  
+    if(confirm('Delete Post?')){
+      http.delete(`http://localhost:3000/posts/${id}`) 
+        .then(data => {
+          ui.showAlert('Post Deleted', 'alert alert-success');  
+          getPosts();  
+        })
+        .catch(err => console.log(err)); 
+        
+    }
+  }
+}
--- section-13-microposts-project-CRUD-frontend/src/http.js
+++ section-13-microposts-project-CRUD-frontend/src/http.js
@@ -1,45 +1,44 @@
 class EasyHTTP {
   async get(url) {
-      const response = await fetch(url);
-      const responseData = await response.json();
-      return responseData;
+    const response = await fetch(url);
+    const responseData = await response.json();
+    return responseData;
   }
 
   async post(url, data) {
-      const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-              'Content-type': 'application/json',
-          },
-          body: JSON.stringify(data),
-      });
-      const resData = await response.json();
-      return resData;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json();
+    return resData;
   }
 
   async put(url, data) {
-      const response = await fetch(url, {
-          method: 'PUT',
-          headers: {
-              'Content-type': 'application/json',
-          },
-          body: JSON.stringify(data),
-      });
-      const resData = await response.json();
-      return resData;
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await response.json();
+    return resData;
   }
 
   async delete(url) {
-      const response = await fetch(url, {
-          method: 'DELETE',
-          headers: {
-              'Content-type': 'application/json',
-          },
-          body: JSON.stringify(data),
-      });
-      const resData = await 'RESOURCE DELETED ✅';
-      return resData;
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const resData = 'RESOURCE DELETED ✅';
+    return resData;
   }
 }
 
-export const http = new EasyHTTP(); 
+export const http = new EasyHTTP();
